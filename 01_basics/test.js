4eb031622b6c4b9f5bd04dd70fdbe43141e43db6
Abhijay2148 <108118484+Abhijay2148@users.noreply.github.com>
2025-05-17 07:20:24 +0000

    01_basics changes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+const accountState = "Jaipur";
+var password = 2121212;
+let accountName = "Abhijay";
+accountEmail = "abab@google.com";
+
+// accountState="delhi";
+
+console.log(accountState);
+
+password = 566556;
+accountName = "Chouhan";
+accountEmail = "cdcd@google.com";
+
+
+console.table([accountState, password, accountEmail, accountName])
+
