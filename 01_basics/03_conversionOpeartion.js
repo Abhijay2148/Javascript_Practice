f544662e1fd42cd4360a967c29a295179ed01f26
Abhijay2148 <108118484+Abhijay2148@users.noreply.github.com>
2025-05-18 06:35:57 +0000

    Conversion Of dataTypes

---
+++ 01_basics/03_conversionOpeartion.js
@@ -0,0 +1,41 @@
+let num=false;
+let num2 = 33;
+
+// console.log(num == num2);
+// console.log(num === num2);
+
+//console.log(typeof num);
+//console.log(typeof (num));
+
+let score=Number(num);
+
+//console.log(typeof(score));
+
+//console.log(score);
+
+// "33" => 33;
+// "33abc" => NaN;
+// null => 0;
+// true => 1;
+// false => 0;
+
+
+let isLoggedin = 1;
+
+let booleanLoggedin = Boolean(isLoggedin);
+
+// console.log(booleanLoggedin);
+
+/*
+1 => true , 0=>false 
+ "" = > false 
+"ABhijay" => true
+*/
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+
+console.log(typeof(stringNumber));
