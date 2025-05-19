3d5e195e02262570518ecac318f175e309641da2
Abhijay2148 <108118484+Abhijay2148@users.noreply.github.com>
2025-05-19 06:58:52 +0000

    maths

---
+++ 01_basics/07_nums_math.js
@@ -0,0 +1,45 @@
+const score = 100;
+
+// console.log(score);
+
+const num = new Number(300);
+
+// console.log(num);
+
+// console.log(num.toString().length);
+
+// console.log(num.toFixed(2));
+
+const otherNumber = 23.92992;
+
+// console.log(otherNumber.toPrecision(4));
+
+
+const hundreds = 2000000;
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//---------  (MATHS)   --------------
+
+// console.log(Math);
+
+// console.log(Math.abs(-8));
+
+// console.log(Math.floor(5.9));
+
+// console.log(Math.ceil(4.2));
+
+// console.log(Math.round(4.5));
+
+// console.log(Math.pow(2,5));
+
+// console.log(Math.sqrt(64));
+
+
+console.log(Math.round(Math.random()*10));
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
