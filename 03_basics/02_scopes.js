eb44c6c2038e67502eeba05586d6648eee551dd9
Abhijay2148 <108118484+Abhijay2148@users.noreply.github.com>
2025-06-23 06:28:52 +0000

    Scope level and mini Hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,11 +6,67 @@ if (true) {       //Blocked scope
     let a=10;
     const b=200;
     
-    console.log("Inner Scope: " + a);
+    // console.log("Inner Scope: " + a);
     
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+//Neted function
+
+function one(){
+    const username = "Abhijay";
+
+    function two()
+    {
+        const website = "Youtube";
+        // console.log(username);
+        
+    }
+
+    // console.log(website);
+
+    two()
+    
+}
+
+// one()
+
+if(true)
+{
+    const username="Abhijay";
+
+    if(true)
+    {
+        const website = "Youtube";
+
+        console.log(username + website);
+        
+    }
+
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++++++++++++  INTRESTING ++++++++++++++++++++++++++++++
+
+
+function addOne(num)
+{
+    return num+1;
+}
+
+addOne(5);
+
+const addTwo = function (num){
+    return num+2;
+}
+
+addTwo(5);
